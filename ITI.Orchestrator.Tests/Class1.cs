01a6e5b72361bf1dc40ef46e024c5c920133acbd
Tristanux <tristanux@gmail.com>
2018-01-30 16:03:33 +0100

    Init

---
+++ ITI.Orchestrator.Tests/Class1.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITI.Orchestrator.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void this_is_a_test()
+        {
+            Assert.That(true);
+        }
+    }
+}
